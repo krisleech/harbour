dde50201ba22ebd26b093744c4b151a289aac918
Kris Leech <kris.leech@interkonect.com>
2012-10-10 10:58:31 +0100

    fix specs, they do not seem to run without these options commented out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,9 @@ require 'harbour'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-  config.order = 'random'
+  # config.run_all_when_everything_filtered = true
+  # config.filter_run :focus
+  # config.order = 'random'
 end
 
 def with_server(port = 8000, &block)
@@ -19,7 +19,7 @@ def start_server!(port)
   return if system "nc -z localhost #{port} > /dev/null"
   server_root = File.join(File.dirname(__FILE__), 'server')
   system "cd #{server_root} && rackup -D -s webrick -p #{port}"
-  sleep(2)
+  sleep(1)
 end
 
 def stop_server!(port)
